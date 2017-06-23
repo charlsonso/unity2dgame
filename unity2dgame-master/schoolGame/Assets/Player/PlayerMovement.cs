0c1d8074620e0a0556bf9d0d6e82f7bf96848de5
Diamond-Dan <dreti.biz@gmail.com>
2017-06-22 23:56:52 -0700

    Add files via upload

---
+++ unity2dgame-master/schoolGame/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // float x = Input.GetAxis("Horizontial") * moveSpeed;
+        //float y = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Translate(Vector2.right * moveSpeed );
+	}
+}
