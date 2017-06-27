b0e72ab0b7c7dc6207c09cfd0f0f3bb0e9d84190
Amy Jiang <amyjianggg@gmail.com>
2017-06-27 16:26:21 -0700

    included jumping script  + zombieMonster
    ISSUE: zombieMonster script does not move 
          --- zombieMonster is not yet a preFab
    cleaned up some of the folders 
    fixed jumping issue for player

---
+++ schoolGame/Assets/Scripts/enemyMovementController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class enemyMovementController : MonoBehaviour {
+
+    //Animations
+    public float enemySpeed;
+    Animator enemyAnimator;
+
+    //Enemy transformations
+    public GameObject enemyGraphic;
+    bool canFlip = true;
+    bool facingRight = false; //starts off looking left
+    float flipTime = 5f; //possiblity that the boar can flip
+    float nextFlipChance = 0f;
+    
+    //Enemy attacking time
+    public float chargeTime;
+    float startChargeTime; //time it starts to run after specific time given in this var
+    bool charging;
+    Rigidbody2D enemyRB;
+
+	// Use this for initialization
+	void Start () {
+        enemyAnimator = GetComponentInChildren<Animator>();
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Time.time > nextFlipChance)
+        {
+            if(Random.Range(0f,10f) >= 1f)
+                flipFacing();
+            nextFlipChance = Time.time + flipTime;
+        }
+	}
+    void onTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "Player") //if tag is the Player
+        {
+            if(facingRight && collider.transform.position.x < transform.position.x)
+            { //if player postion is behind enemy;; face the player
+                flipFacing();
+            } 
+            else if(!facingRight && collider.transform.position.x > transform.position.x)
+            { //if on otherside of enemy
+                flipFacing();
+            }
+            canFlip = false; //once facing the right way ; don't flip
+            charging = true;
+            startChargeTime = Time.time + chargeTime;
+        }
+    }
+    void onTriggerStay2D(Collider2D collider)
+    {//if player is in the trigger range of enemy
+        if(collider.tag == "Player")
+        {
+            if(startChargeTime < Time.time)
+            {
+                if (!facingRight) enemyRB.AddForce(new Vector2(-1, 0)*enemySpeed);
+                else enemyRB.AddForce(new Vector2(1, 0)*enemySpeed);
+                enemyAnimator.SetBool("isCharging", charging);
+            }
+        }
+    }
+    void onTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            canFlip = true;
+            charging = false;
+            enemyRB.velocity = new Vector2(0f,0f);
+            enemyAnimator.SetBool("isCharging", charging);
+        }
+
+    }
+
+    void flipFacing()
+    {
+        if (!canFlip) return;
+        float facingX = enemyGraphic.transform.localScale.x;
+        facingX *= -1f;
+        enemyGraphic.transform.localScale = new Vector3(facingX, enemyGraphic.transform.localScale.y, enemyGraphic.transform.localScale.z);
+        facingRight = !facingRight;
+    }
+}
--- schoolGame/Assets/Scripts/playerController.cs
+++ schoolGame/Assets/Scripts/playerController.cs
@@ -2,89 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
-	//movement variables
-	public float maxSpeed;
+public class playerController : MonoBehaviour
+{
+    //movement variables
+    public float maxSpeed;
+    bool facingRight = true;
+    Rigidbody2D myRB;
+    Animator myAnimator;
+
+    //jumping variables
+    bool grounded = false;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
     public float jumpHeight;
-    //dectect ground
-     bool canJump;
-	//reference to rigid body, changing velocity, etc...
-	Rigidbody2D charRigidBody;
-	//allows us to maniuplate speed
-	Animator anim;
-	bool facingRight;
 
-	// Use this for initialization
-	void Start () {
-		charRigidBody = GetComponent<Rigidbody2D> ();
-		anim = GetComponent<Animator> ();
-		facingRight = true;
-       
-	}
-    void OnCollisionEnter2D(Collision2D coll)
+    // Use this for initialization
+    void Start()
     {
-        float jump = Input.GetAxis("Vertical");
-        // If the Collider2D component is enabled on the collided object
-        if (coll.collider == true)
-        {
-          
-            canJump = true;
-            print(canJump);
-
-        }
-    
-
+        myRB = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
 
     }
 
-    void OnCollisionExit2D(Collision2D coll)
+    void Update()
+    {
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnimator.SetBool("isGrounded", grounded);
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
+    }
+    // Update is called once per frame
+    // always exactly the same time that it is called.
+    //RigidBody means that the object is now affected by physics.
+    // --> Update is called on every single frame doesnt consider frames
+    // --> FixedUpdate is always called on a specific amount of time.
+    void FixedUpdate()
     {
 
-        //if (coll.collider == false)
+        //check if grounded? is it intersecting the ground = true else false
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); //draw a tiny circle that checks param
+        myAnimator.SetBool("isGrounded", grounded);
+
+        myAnimator.SetFloat("verticalSpeed", myRB.velocity.y);
+        float move = Input.GetAxis("Horizontal");
+        myAnimator.SetFloat("speed", Mathf.Abs(move)); //param and give it the value
+
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y); // manipulate x value
+                                                                       // y value is not changed
+        if (move > 0 && !facingRight)
         {
-            canJump = false;
-            print(canJump);
+            flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            flip(); //pressing a and facing left
         }
     }
 
-        // Update is called once per frame
-        //changed to fixedupdate
-        //called after a specific amount of time, all the time
-        //too get physics objects properly you want to manipulate in fixedupdate
-        void FixedUpdate () {
-		//get axis returns value between -1 and 1
-		//getaxis raw will only return -1 0 and 1
-		float move = Input.GetAxis("Horizontal");
-        float jump = Input.GetAxis("Vertical");
-		anim.SetFloat ("speed", Mathf.Abs(move));
-		charRigidBody.velocity = new Vector2(move * maxSpeed, anim.velocity.y);
-     
-
-        if(canJump==true)
-        {
-
-            charRigidBody.AddForce(new Vector2(anim.velocity.x, jump * jumpHeight), ForceMode2D.Force);
-        }
-
-      
-		//need to switch based off if moving right but not facing right
-		if (move > 0 && !facingRight) {
-			flip();
-		} 
-		else if (move < 0 && facingRight) {
-			flip();
-		}
-			
-	}
-
-	void flip(){
-		//need to change the transform scale to negative
-		//switch between if its facing right
-		//
-		facingRight = !facingRight;
-		//creat
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale; // take values and place in scale
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 }
