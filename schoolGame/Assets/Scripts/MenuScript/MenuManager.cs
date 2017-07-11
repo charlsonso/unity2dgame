a9dd1706d2ca1b241a23805481c86b46ed32d2a4
dschulz474 <dschulz474@student.glendale.edu>
2017-07-10 21:02:18 -0700

    Menu

--- schoolGame/Assets/Scripts/MenuScript/MenuManager.cs
+++ schoolGame/Assets/Scripts/MenuScript/MenuManager.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void ToGame()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
 }
